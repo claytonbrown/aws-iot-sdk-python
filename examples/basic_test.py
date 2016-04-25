42a45b89c30e5890129208dfcc5ce8eff5f0f36a
CLayton Brown <dev@claytonbrown.com>
2016-04-25 17:52:00 +1000

    adding new certs for testing

--- examples/basic_test.py
+++ examples/basic_test.py
@@ -63,6 +63,7 @@ def connected():
 
 
 def all_off():
+    """
     pinMode(led1, "OUTPUT")
     pinMode(led2, "OUTPUT")
     pinMode(led3, "OUTPUT")
@@ -71,6 +72,8 @@ def all_off():
     digitalWrite(led1, 1)             # Send HIGH to switch on LED
     digitalWrite(led2, 1)             # Send HIGH to switch on LED
     digitalWrite(led3, 1)             # Send HIGH to switch on LED
+    """
+    print("all-off invoked")
 
 
 def on_connect(client, userdata, flags, rc):
@@ -136,7 +139,7 @@ def switcher():
 # vvvvvvvvvv
 # BEGIN
 # ----------
-startup()
+# startup()
 # ^^^^^^^^^^^
 
 
--- examples/config.py
+++ examples/config.py
@@ -8,3 +8,16 @@ rootca = "./examples/certs/VeriSign-Class 3-Public-Primary-Certification-Authori
 privatekey = "./examples/certs/4a6ddf7be8-private.pem.key"
 publickey = "./examples/certs/4a6ddf7be8-public.pem.key"
 cert = "./examples/certs/4a6ddf7be8-certificate.pem.crt"
+
+"""
+// Get from console
+// =================================================
+#define AWS_IOT_MQTT_HOST              "A1YWW1UJGZMYMX.iot.us-east-1.amazonaws.com"
+#define AWS_IOT_MQTT_PORT              8883
+#define AWS_IOT_MQTT_CLIENT_ID         "SampleThing"
+#define AWS_IOT_MY_THING_NAME          "SampleThing"
+#define AWS_IOT_ROOT_CA_FILENAME      "VeriSign-Class 3-Public-Primary-Certification-Authority-G5.pem"
+#define AWS_IOT_CERTIFICATE_FILENAME   "4a6ddf7be8-certificate.pem.crt"
+#define AWS_IOT_PRIVATE_KEY_FILENAME   "4a6ddf7be8-private.pem.key"
+// =================================================
+"""
